8a37752e4d2ecfd17f6e6303d91be9b3af013f8d
AnastasiaKozhemyachko <47752444+AnastasiaKozhemyachko@users.noreply.github.com>
2019-03-11 17:06:03 +0300

    Update max-heap.js

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,11 +2,11 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+        this.root = null;
+        this.parentNodes =[];
 	}
 
 	push(data, priority) {
-		
 	}
 
 	pop() {
@@ -34,6 +34,42 @@ class MaxHeap {
 	}
 
 	insertNode(node) {
+        if(this.root == null){
+            this.root = node;
+            this.parentNodes =[node];
+            return;
+        }
+            if(this.root.left == null){
+                this.root.left = node;
+                this.parentNodes = [this.root, node];
+                return;  
+            }
+            if(this.root.right == null){
+                this.root.right = node;
+                this.parentNodes = [this.root.left, node];
+                return;   
+            }
+            if(this.root.left.left == null){
+                this.root.left.left = node;
+                this.parentNodes =[this.root.left, this.root.right, node];
+                return;  
+            }
+            if(this.root.left.right == null){
+                this.parentNodes =[this.root.right, this.root.left.left, node];
+                this.root.left.right = node;
+                return;  
+            }
+            if(this.root.right.left == null){
+                this.parentNodes =[this.root.right, this.root.left.left, this.root.left.right, node];
+                this.root.right.left = node;
+                return;   
+            }
+            if(this.root.right.right == null){
+                this.parentNodes =[this.root.left.left, this.root.left.right, this.root.right.left, node];
+                this.root.right.right = node;
+                return;   
+       	    }
+
 		
 	}
 
