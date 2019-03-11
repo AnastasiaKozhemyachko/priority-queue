da2e26ba18065be3d2de28f02fdeb2d320fd9225
AnastasiaKozhemyachko <47752444+AnastasiaKozhemyachko@users.noreply.github.com>
2019-03-11 18:41:26 +0300

    Update max-heap.js

--- src/max-heap.js
+++ src/max-heap.js
@@ -7,6 +7,9 @@ class MaxHeap {
 	}
 
 	push(data, priority) {
+        // const node = new Node(data, priority);
+		// insertNode(node);
+        // shiftNodeUp(node);
 	}
 
 	pop() {
@@ -26,55 +29,88 @@ class MaxHeap {
 	}
 
 	isEmpty() {
-		
+        // if(this.root == null && this.parentNodes == [])
+        // return true;
+        // else 
+        // return false;
 	}
 
 	clear() {
-		
+        this.root = null;
+        this.parentNodes = []; 
 	}
-
+//вставить узел
 	insertNode(node) {
         if(this.root == null){
             this.root = node;
-            this.parentNodes =[node];
+            this.parentNodes =[];
+            if(this.root.left == null){
+                this.parentNodes = [this.root];
+
+            }
+            if(this.root.right == null){
+                this.parentNodes = [this.root, this.root.left];
+                return;  
+            }
+            if(this.root.left.left == null){
+                this.parentNodes = [this.root.left, this.root.right];
+                return;   
+            }
+            if(this.root.right.left == null){
+                this.parentNodes =[this.root.left, this.root.right, this.root.left.left];
+                return;  
+            }
+            if(this.root.right.right == null){
+                this.parentNodes =[this.root.right, this.root.left.left, this.root.left.right];
+                return;  
+            }
             return;
         }
             if(this.root.left == null){
                 this.root.left = node;
-                this.parentNodes = [this.root, node];
+                this.parentNodes =[];
+                this.parentNodes = [this.root, this.root.left];
                 return;  
             }
             if(this.root.right == null){
                 this.root.right = node;
-                this.parentNodes = [this.root.left, node];
+                this.parentNodes =[];
+                this.parentNodes = [this.root.left, this.root.right];
                 return;   
             }
             if(this.root.left.left == null){
                 this.root.left.left = node;
-                this.parentNodes =[this.root.left, this.root.right, node];
+                this.parentNodes =[];
+                this.parentNodes =[this.root.left, this.root.right, this.root.left.left];
                 return;  
             }
             if(this.root.left.right == null){
-                this.parentNodes =[this.root.right, this.root.left.left, node];
                 this.root.left.right = node;
+                this.parentNodes =[];
+                this.parentNodes =[this.root.right, this.root.left.left, this.root.left.right];
                 return;  
             }
             if(this.root.right.left == null){
-                this.parentNodes =[this.root.right, this.root.left.left, this.root.left.right, node];
                 this.root.right.left = node;
+                this.parentNodes =[];
+                this.parentNodes =[this.root.right, this.root.left.left, this.root.left.right, this.root.right.left];
                 return;   
             }
             if(this.root.right.right == null){
-                this.parentNodes =[this.root.left.left, this.root.left.right, this.root.right.left, node];
                 this.root.right.right = node;
-                return;   
-       	    }
-
-		
+                this.parentNodes =[];
+                this.parentNodes =[this.root.left.left, this.root.left.right, this.root.right.left, this.root.right.right];
+                return; 
+            }
 	}
 
 	shiftNodeUp(node) {
-		
+
+        while (node.parent!=null && node.priority>node.parent.priority){
+            node.swapWithParent();
+        }
+                this.clear();
+                this.insertNode(node);
 	}
 
 	shiftNodeDown(node) {
